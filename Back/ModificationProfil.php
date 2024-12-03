3df6d26c28590eb08113f75b88eaf3d81c3c7486
Theo Enfedaque <t.enfedaque@lprs.fr>
2024-12-03 09:34:49 +0100

    feat( modfif) : non fonctionnel

--- Back/ModificationProfil.php
+++ Back/ModificationProfil.php
@@ -1,1 +1,49 @@
 <?php
+$bdd = include "../BDD/BDD.php";
+
+$nom = $_POST['nom'];
+$prenom = $_POST['prenom'];
+$email = $_POST['email'];
+$tel_fixe = $_POST['tel_fixe'];
+$tel_portable = $_POST['tel_portable'];
+$rue = $_POST['rue'];
+$cp = $_POST['cp'];
+$ville = $_POST['ville'];
+$mdp = $_POST['mdp'];
+/*
+$reqVerif = $bdd->prepare('SELECT * FROM inscrit WHERE email = :email');
+$reqVerif->execute(array('email' => $email));
+$resVerif = $reqVerif->fetch();
+if (!$resVerif || $resVerif['email'] != $email) {
+    $reqModif = $bdd->prepare('UPDATE inscrit SET nom = :nom, prenom = :prenom, email = :email, tel_fixe = :tel_fixe, tel_portable = :tel_portable, rue = :rue, cp = :cp, ville = :ville, mdp = :mdp WHERE id_inscrit = :id_inscrit');
+    $reqModif->execute(array(
+        "nom" => $nom,
+        "prenom" => $prenom,
+        "email" => $email,
+        "tel_fixe" => $tel_fixe,
+        "tel_portable" => $tel_portable,
+        "rue" => $rue,
+        "cp" => $cp,
+        "ville" => $ville,
+        "mdp" => $mdp,
+        "id_inscrit" => $_SESSION['id_user']
+    ));
+    header('Location: ../Front/PageMembre.php?Modif =1');
+}else{
+    header('Location: ../Front/PageMembre.php?error=email');
+}*/
+$reqModif = $bdd->prepare('UPDATE inscrit SET nom = :nom, prenom = :prenom, email = :email, tel_fixe = :tel_fixe, tel_portable = :tel_portable, rue = :rue, cp = :cp, ville = :ville, mdp = :mdp WHERE id_inscrit = :id_inscrit');
+$reqModif->execute(array(
+    "nom" => $nom,
+    "prenom" => $prenom,
+    "email" => $email,
+    "tel_fixe" => $tel_fixe,
+    "tel_portable" => $tel_portable,
+    "rue" => $rue,
+    "cp" => $cp,
+    "ville" => $ville,
+    "mdp" => $mdp,
+    "id_inscrit" => $_SESSION['id_user']
+));
+header('Location: ../Front/PageMembre.php?Modif=1');
+
--- Front/PageMembre.php
+++ Front/PageMembre.php
@@ -5,8 +5,12 @@
     <title>Page Membre</title>
 </head>
 <?php
-// role admin = 0 role membre = 1
+$bdd = include "../BDD/BDD.php";
+// role admin = 0 role membre = NULL
 session_start();
+$reqVerif = $bdd->prepare('SELECT * FROM inscrit WHERE id_inscrit = :id_inscrit');
+$reqVerif->execute(array('id_inscrit' => $_SESSION['id_user']));
+$resVerif = $reqVerif->fetch();
 ?>
 <body>
 <p>Bienvenue <?=$_SESSION['user']?></p>
@@ -28,60 +32,76 @@ session_start();
     <tr>
         <td>
             <label for="nom"> Nom : </label>
-            <input type="text" id="nom" name="nom" max="50">
+            <input type="text" id="nom" name="nom" max="50" value=<?= $resVerif['nom'] ?>>
         </td>
     </tr>
     <tr>
         <td>
             <label for="prenom"> Prenom : </label>
-            <input type="text" id="prenom" name="prenom" max="50">
+            <input type="text" id="prenom" name="prenom" max="50" value=<?= $resVerif['prenom'] ?>>
         </td>
     </tr>
     <tr>
         <td>
             <label for="emaili"> Mail : </label>
-            <input type="email" id="emaili" name="emaili" max="100">
+            <input type="email" id="email" name="email" max="100" value=<?= $resVerif['email'] ?>>
         </td>
     </tr>
     <tr>
         <td>
             <label for="tel_fixe"> Téléphone fixe : </label>
-            <input type="text" id="tel_fixe" name="tel_fixe" max="15">
+            <input type="text" id="tel_fixe" name="tel_fixe" max="15" value=<?= $resVerif['tel_fixe'] ?>>
         </td>
     </tr>
     <tr>
         <td>
             <label for="tel_portable"> Téléphone portable : </label>
-            <input type="text" id="tel_portable" name="tel_portable" max="15">
+            <input type="text" id="tel_portable" name="tel_portable" max="15" value=<?= $resVerif['tel_portable'] ?>>
         </td>
     </tr>
     <tr>
         <td>
             <label for="rue">Rue : </label>
-            <input type="text" id="rue" name="rue" max="80">
+            <input type="text" id="rue" name="rue" max="80" value=<?= $resVerif['rue'] ?>>
         </td>
     </tr>
     <tr>
         <td>
             <label for="cp">Code postal : </label>
-            <input type="text" id="cp" name="cp" max="5">
+            <input type="text" id="cp" name="cp" max="5" value=<?= $resVerif['cp'] ?>>
         </td>
     </tr>
     <tr>
         <td>
             <label for="ville">Ville : </label>
-            <input type="text" id="ville" name="ville" max="50">
+            <input type="text" id="ville" name="ville" max="50" value=<?= $resVerif['ville'] ?>>
         </td>
     </tr>
     <tr>
         <td>
             <label for="mdpi">Mot de passe : </label>
-            <input type="password" id="mdpi" name="mdpi" max="50" required>
+            <input type="password" id="mdp" name="mdp" max="50" value=<?= $resVerif['mdp'] ?>>
         </td>
     </tr>
     <tr>
         <td>
             <input type="submit" name="Modification" value="Modifier">
+        </td>
+    </tr>
+    <tr>
+        <td>
+            <?php
+            if (isset($_GET['Modif'])) {
+                echo "
+                Vos modification ont bien été pris en compte.
+                ";
+            }
+            if (isset($_GET['error'])) {
+                echo "
+                L'addresse email que vous voulez existe déjà. 
+                ";
+            }
+            ?>
         </td>
     </tr>
 </table>
