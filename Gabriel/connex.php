c10da6cff14f5f13902463031ab0d640ee95d4f9
Berynd <gabriel.berinde93@icloud.com>
2024-12-10 17:03:51 +0100

    feat: ficher gabriel

--- Gabriel/connex.php
+++ Gabriel/connex.php
@@ -1,6 +1,24 @@
-<form action="login.html" method="post">
 <?php
 
-$bdd = new PDO('mysql:host=localhost:8889;dbname=test;charset=utf8',
+$bdd = new PDO('mysql:host=localhost:8889;dbname=tp1;charset=utf8'
+    ,
     'root',
     'root');
+
+$email = $_POST['email'];
+$mdp = $_POST['mdp'];
+
+$reqConnexion = $bdd->prepare('SELECT * FROM inscrit WHERE email = :email AND mdp = :mdp');
+$reqConnexion->execute(array('email' => $email, 'mdp' => $mdp));
+$resConnexion = $reqConnexion->fetch();
+var_dump($resConnexion);
+var_dump($email, $mdp);
+if ($resConnexion==null) {
+    echo "t nul ff15, nice tuto";
+}
+else{
+    session_start();
+    $_SESSION['user'] = $resConnexion['email'];
+    header("Location: acceuil.php");
+
+}
